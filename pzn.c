39c963363eb8af56d5709eb480fcebbab5f2a621
vijayaram55555 <30368749+vijayaram55555@users.noreply.github.com>
2017-08-22 17:00:19 +0530

    Create pzn.c

---
+++ pzn.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+if(n==0)
+{
+printf("num is zero");
+}
+else if(n>=1)
+{
+printf("num is positive");
+}
+else 
+{
+printf("num is negative");
+}
+}
