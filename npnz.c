68faeac4fe38b701431285ef60d86dd1665534d0
vijayaram55555 <30368749+vijayaram55555@users.noreply.github.com>
2017-09-20 20:14:52 +0530

    Create npnz.c

---
+++ npnz.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+if(n==0)
+printf("the num is zero");
+else if(n>1)
+printf("the num is positive");
+else if(n<1)
+printf("the num is negative");
+}
